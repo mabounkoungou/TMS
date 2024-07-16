0f1f485294bc5e829ef21d86e6bd7b4127080503
raad <raadbouks@gmail.com>
2024-07-16 15:18:32 +0000

    initial commit

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<footer class="main-footer">
+  <div class="container">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <footer>
+      <strong>Copyright &copy; <span id="year"></span> <a href="#">TMS</a></strong>
+    </footer>
+    <script>
+      document.getElementById("year").textContent = new Date().getFullYear();
+    </script>
+  </div>
+  <!-- /.container -->
+</footer>
